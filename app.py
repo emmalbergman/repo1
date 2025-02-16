b949cf145b24656abe77a1dac9b4f342f116c3e5
TheodoreBagley <teddybagley18@gmail.com>
2025-02-16 16:22:21 -0700

    fixing previous push

--- app.py
+++ app.py
@@ -25,7 +25,10 @@ def _db_close(exc):
 #NOTE: my IDE made me do it this way, can change to app.get if broken
 @app.get("/")
 def home():
-    products = Product.all()
+    # Fills the days left for each product with product.get_days_until_out
+    Product.fill_days_left()
+    # Loads products in urgency order
+    products = Product.urgency_rank()
     return render_template("index.html", product_list=products)
 
 @app.get("/inventory-history")
@@ -33,13 +36,13 @@ def inventory_history():
     product_id = request.args.get('product-id', None, type=int)
     if product_id is None: # TODO: have actual error page
         return abort(404, description=f"Could not find product id")
-    
+
     product = Product.get_product(product_id)
     if product is None: # TODO: have actual error page
         return abort(404, description=f"Could not find product {product_id}")
-    
+
     snapshots = InventorySnapshot.all_of_product(product_id)
-    usage = product.get_usage_per_day() 
+    usage = product.get_usage_per_day()
     days_until_out = product.get_days_until_out(usage)
 
     return render_template(
@@ -60,8 +63,8 @@ def add():
     products = Product.all()
     count = len(products)
     stuff = "stuff" + str(count)
-    Product.add_product(stuff, 5, 5.00, "piles", 0, "")
-
+    Product.add_product(stuff, 5, 5.00, "piles", 0, 10)
+    Product.fill_days_left()
     return redirect("/")
 
 @app.route("/update/inventory/<int:product_id>", methods=["PATCH"])
@@ -69,7 +72,7 @@ def update_inventory(product_id: int):
     new_stock = request.form.get('stock', None, type=int)
     if new_stock is None or new_stock < 0:
         return abort(400, description="Stock count must be a positive integer")
-    
+
     product = Product.get_product(product_id)
     if product is None:
         return abort(404, description=f"Could not find product {product_id}")
@@ -82,7 +85,7 @@ def update_inventory(product_id: int):
 
 if __name__ == '__main__':
     #TODO: disable debug mode when we are done developing
-    Product.add_product("shampoo", 5, 5.00,"bottles", 10, "")
-    Product.add_product("stuff", 5, 5.00, "piles", 0, "")
+    Product.add_product("shampoo", 5, 5.00,"bottles", 10, None)
+    Product.add_product("stuff", 5, 5.00, "piles", 0, None)
     Product.fill_days_left()
     app.run(port=5000, debug=True)
