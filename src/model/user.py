5d646ddc69ca118030a159e3ab31eee92ab0e0da
CodyHager <chager004@gmail.com>
2025-02-16 21:37:31 -0700

    get started on access control

---
+++ src/model/user.py
@@ -0,0 +1,34 @@
+from peewee import *
+from flask_security import UserMixin, RoleMixin
+
+user_db = SqliteDatabase('users.db')
+
+class Role(RoleMixin, user_db.Model):
+    name = CharField(unique=True)
+    description = TextField(null=True)
+    permissions = TextField(null=True)
+
+# N.B. order is important since db.Model also contains a get_id() -
+# we need the one from UserMixin.
+class User(UserMixin, user_db.Model):
+    email = TextField()
+    password = TextField()
+    active = BooleanField(default=True)
+    fs_uniquifier = TextField(null=False)
+    confirmed_at = DateTimeField(null=True)
+
+class UserRoles(user_db.Model):
+    # Because peewee does not come with built-in many-to-many
+    # relationships, we need this intermediary class to link
+    # user to roles.
+    user = ForeignKeyField(User, related_name='roles')
+    role = ForeignKeyField(Role, related_name='users')
+    name = property(lambda self: self.role.name)
+    description = property(lambda self: self.role.description)
+
+    def get_permissions(self):
+        return self.role.get_permissions()
+
+
+    class Meta:
+        database = user_db
